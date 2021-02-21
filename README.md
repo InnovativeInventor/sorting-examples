1e2c9d864d873e4a7352ca9c8d7f4cf9c8c6fdaa
Max Fan <theinnovativeinventor@gmail.com>
2021-02-21 16:55:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## All the sorts
+An example implementation of a variety of sorting algos for fun in Rust.
+
+
+## Benchmarking
+Example usage:
+``` sh
+./benchmark
+time cat benchmark-8.txt | ./sort --bubble
+```
+
---
+++ benchmark/src/main.rs
@@ -0,0 +1,31 @@
+#![feature(rustc_private)]
+extern crate rand;
+use crate::rand::Rng;
+
+use rand::rngs::StdRng;
+use rand::SeedableRng;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    for i in 2..10 {
+        let min: i64 = 0;
+        let max: i64 = i64::pow(10, i);
+
+        let mut rng = StdRng::seed_from_u64(42); // the answer to life
+
+        let mut nums = vec![];
+
+        for _ in min..max {
+            let n: i64 = rng.gen_range(min, max);
+            nums.push(n);
+        }
+
+        let mut buffer = File::create(format!("benchmark-{i}.txt", i = i)).unwrap();
+        for num in nums {
+            buffer
+                .write((num.to_string() + "\n").as_bytes())
+                .expect("Failed to write to buffer");
+        }
+    }
+}
