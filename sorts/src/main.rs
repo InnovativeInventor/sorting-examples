03c00612ba1762cc86b087681e2931417b56b958
Max Fan <theinnovativeinventor@gmail.com>
2021-02-23 10:27:26 -0500

    Add insertion sort example

---
+++ sorts/src/main.rs
@@ -0,0 +1,47 @@
+use std::io::BufRead;
+use std::collections::LinkedList;
+
+fn main() {
+    let input = std::io::stdin();
+
+    let mut sorter = InsertionSortVec::new(usize::pow(10, 6));
+
+    for line in input.lock().lines(){
+        let num: i64= line.unwrap().parse().unwrap();
+        sorter.insert(num);
+    }
+
+    for integer in &sorter.list {
+        println!("{}", integer)
+    }
+
+}
+
+trait Sort {
+    fn insert(&mut self, input: i64);
+    fn new(cap: usize) -> Self;
+}
+
+struct InsertionSortVec {
+    list: Vec<i64>
+}
+
+impl Sort for InsertionSortVec {
+    fn new(cap: usize) -> Self {
+        Self {
+            list: Vec::<i64>::with_capacity(cap),
+        }
+    }
+    fn insert(&mut self, input: i64) {
+        if self.list.is_empty() {
+            self.list.push(input);
+        } else {
+            for count in 0..self.list.len() {
+                if &input < &self.list[count] { // invariant holds
+                    self.list.insert(count, input);
+                    break
+                }
+            }
+        }
+    }
+}
