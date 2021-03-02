a838912fb66595b3542d767215acd45c0600ec4c
Max Fan <theinnovativeinventor@gmail.com>
2021-03-02 11:01:48 -0500

    Some more formatting for benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,25 @@
+cd aiyu-sorts
+cargo build --release -q
+echo "Aiyu benchmarks:"
+time cat ../benchmark/benchmark-5.txt | ./target/release/aiyu-sorts > /dev/null
+
+cd ..
+echo
+
+cd max-sorts
+cargo build --release -q
+echo "Max benchmarks:"
+echo "Max -- default"
+time cat ../benchmark/benchmark-5.txt | ./target/release/max-sorts -d > /dev/null
+echo "Max -- quicksort"
+time cat ../benchmark/benchmark-5.txt | ./target/release/max-sorts -q > /dev/null
+echo "Max -- insert"
+time cat ../benchmark/benchmark-5.txt | ./target/release/max-sorts -i > /dev/null
+echo "Max -- mergesort"
+time cat ../benchmark/benchmark-5.txt | ./target/release/max-sorts -m > /dev/null
+
+cd ..
+
+echo "Default sorting algo in Linux:"
+time cat ../benchmark/benchmark-5.txt | sort -g > /dev/null
+
