320a4d5ff36edda80f0939283a6cf9c40b3a825b
“Spencer <sbowles22@choate.edu>
2021-03-03 14:59:02 -0500

    add size arg to benchmark.sh -s

--- benchmark.sh
+++ benchmark.sh
@@ -1,12 +1,14 @@
+# arg1: 2..10 is valid, recommended 4-6
+
 cd aiyu-sorts
 cargo build --release -q
 echo "Aiyu benchmarks:"
 echo "Aiyu -- quicksort"
-time cat ../benchmark/benchmark-5.txt | ./target/release/aiyu-sorts -q | shasum
+time cat ../benchmark/benchmark-$1.txt | ./target/release/aiyu-sorts -q | shasum
 echo "Aiyu -- mergesort"
-time cat ../benchmark/benchmark-5.txt | ./target/release/aiyu-sorts -m | shasum
-echo "Max -- heapsort"
-time cat ../benchmark/benchmark-5.txt | ./target/release/aiyu-sorts -h | shasum
+time cat ../benchmark/benchmark-$1.txt | ./target/release/aiyu-sorts -m | shasum
+echo "Aiyu -- heapsort"
+time cat ../benchmark/benchmark-$1.txt | ./target/release/aiyu-sorts -h | shasum
 
 cd ..
 echo
@@ -15,13 +17,13 @@ cd max-sorts
 cargo build --release -q
 echo "Max benchmarks:"
 echo "Max -- default"
-time cat ../benchmark/benchmark-5.txt | ./target/release/max-sorts -d | shasum
+time cat ../benchmark/benchmark-$1.txt | ./target/release/max-sorts -d | shasum
 echo "Max -- quicksort"
-time cat ../benchmark/benchmark-5.txt | ./target/release/max-sorts -q | shasum
+time cat ../benchmark/benchmark-$1.txt | ./target/release/max-sorts -q | shasum
 echo "Max -- insert"
-time cat ../benchmark/benchmark-5.txt | ./target/release/max-sorts -i | shasum
+time cat ../benchmark/benchmark-$1.txt | ./target/release/max-sorts -i | shasum
 echo "Max -- mergesort"
-time cat ../benchmark/benchmark-5.txt | ./target/release/max-sorts -m | shasum
+time cat ../benchmark/benchmark-$1.txt | ./target/release/max-sorts -m | shasum
 
 cd ..
 echo
@@ -30,16 +32,17 @@ cd spencer-sorts
 cargo build --release -q
 echo "Spencer Benchmarks"
 echo "Spencer -- Insertion"
-time cat ../benchmark/benchmark-5.txt | ./target/release/spencer-sorts -i | shasum
+time cat ../benchmark/benchmark-$1.txt | ./target/release/spencer-sorts -i | shasum
 echo "Spencer -- Quick"
-time cat ../benchmark/benchmark-5.txt | ./target/release/spencer-sorts -q | shasum
+time cat ../benchmark/benchmark-$1.txt | ./target/release/spencer-sorts -q | shasum
 echo "Spencer -- Radix"
-time cat ../benchmark/benchmark-5.txt | ./target/release/spencer-sorts -r | shasum
+time cat ../benchmark/benchmark-$1.txt | ./target/release/spencer-sorts -r | shasum
 echo "Spencer -- Bubble"
-time cat ../benchmark/benchmark-5.txt | ./target/release/spencer-sorts -b | shasum
+time cat ../benchmark/benchmark-$1.txt | ./target/release/spencer-sorts -b | shasum
 echo "Spencer -- Transposition"
-time cat ../benchmark/benchmark-5.txt | ./target/release/spencer-sorts -t | shasum
+time cat ../benchmark/benchmark-$1.txt | ./target/release/spencer-sorts -t | shasum
+echo "Spencer -- Xtreme"
+time cat ../benchmark/benchmark-$1.txt | ./target/release/spencer-sorts -x | shasum
 
 echo "Default sorting algo in Linux:"
-time cat ../benchmark/benchmark-5.txt | sort -g | shasum
-
+time cat ../benchmark/benchmark-$1.txt | sort -g | shasum
